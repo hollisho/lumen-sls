9cc1fa3a13bc50aac311a60b55b9e3d24b1491a9
Hollis <he_wenzhi@126.com>
2020-05-24 14:55:22 +0800

    use buffer

--- src/Logging/SLSFormatter.php
+++ src/Logging/SLSFormatter.php
@@ -3,20 +3,25 @@
 namespace hollisho\lumensls\Logging;
 
 use Monolog\Handler\BufferHandler;
+use Monolog\Logger;
 
 class SLSFormatter
 {
     /**
-     * @param  \Illuminate\Log\Logger  $logger
-     * @return void
+     * @author Hollis Ho <he_wenzhi@126.com>
+     * @param $logger
+     * @param int $bufferLimit
+     * @param int $level
+     * @param bool $bubble
+     * @param bool $flushOnOverflow
      */
-    public function __invoke($logger)
+    public function __invoke($logger, $bufferLimit = 0, $level = Logger::DEBUG, $bubble = true, $flushOnOverflow = false)
     {
         /* @var $slsLog \hollisho\lumensls\SLSLog */
         $slsLog = app('sls');
         /* @var $logger \Monolog\Logger */
         $handler = new SLSLogHandler($slsLog);
         $handler->setFormatter(new SLSLogContentFormatter());
-        $logger->pushHandler(new BufferHandler($handler, 1));
+        $logger->pushHandler(new BufferHandler($handler, $bufferLimit, $level, (bool) $bubble, (bool) $flushOnOverflow));
     }
 }
