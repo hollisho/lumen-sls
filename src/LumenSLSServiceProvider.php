1e07b46c5d87944818ac6f71459d510095253e7d
Hollis_Ho <he_wenzhi@126.com>
2020-07-31 20:46:46 +0800

    lumen5.8

--- src/LumenSLSServiceProvider.php
+++ src/LumenSLSServiceProvider.php
@@ -5,26 +5,9 @@ namespace hollisho\lumensls;
 use Aliyun\SLS\Client;
 use hollisho\lumensls\Console\PublishConfigCommand;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Lumen\Application;
 
 class LumenSLSServiceProvider extends ServiceProvider
 {
-
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    public function boot()
-    {
-        /* @var Application $app */
-//        $app = $this->app;
-//        $app->configure('sls');
-    }
-
-
     /**
      * Add the connector to the queue drivers.
      *
@@ -40,27 +23,22 @@ class LumenSLSServiceProvider extends ServiceProvider
             'command.lumen-sls.publish-config'
         );
 
-        $this->app->singleton('sls', function ($app) {
-            $config = $app['config']['sls'];
-            $accessKeyId     = array_get($config, 'access_key_id');
-            $accessKeySecret = array_get($config, 'access_key_secret');
-            $endpoint        = array_get($config, 'endpoint');
-            $project         = array_get($config, 'project');
-            $store           = array_get($config, 'log_store');
-            $topic           = array_get($config, 'topic');
-//            $env             = array_get($config, 'env');
+        $this->app->singleton('sls', function () {
+            $accessKeyId     = config('sls.access_key_id');
+            $accessKeySecret = config('sls.access_key_secret');
+            $endpoint        = config('sls.endpoint');
+            $project         = config('sls.project');
+            $store           = config('sls.log_store');
+            $topic           = config('sls.topic');
+//            $env             = config('sls.env');
 
-            $client = new Client($endpoint, $accessKeyId, $accessKeySecret);
-
-            $log = new SLSLogManager($client, $topic);
+            $log = new SLSLogManager(new Client($endpoint, $accessKeyId, $accessKeySecret), $topic);
             $log->setProject($project);
             $log->setLogStore($store);
 
             return $log;
         });
 
-        $config = $this->app['config']['sls'];
-
-        $this->app->instance('sls.writer', new SLSLogWriter(app('sls'), $this->app['events'], $config['topic'], $config['env']));
+        $this->app->instance('sls.writer', new SLSLogWriter(app('sls'), $this->app['events'], config('sls.topic'), config('sls.env')));
     }
 }
